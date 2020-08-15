c939b2d17bdabc412685f2c00d0d52479af9069d
Behzad Fazeli <fazelasl@yahoo.com>
2020-08-15 15:03:55 -0400

    oops! i left the credentials in the open

--- ssh.sh
+++ ssh.sh
@@ -2,7 +2,7 @@ read serverName
 
 if [ 'sky' = $serverName ]
 then
-sshpass -p '5clUW4gf' ssh skywriters@144.76.143.167 -p 49150
+sshpass -p '*******' ssh skywriters@144.76.143.167 -p *****
 else
 echo "no such server exists in our database!"
 fi
