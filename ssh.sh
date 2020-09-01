07dada3d1343d6f5df4fb8413c78e33318426cde
Behzad Fazelasl <behzad.fazelasl@gmail.com>
2020-09-01 00:25:19 -0400

    add new feature to delete and edit existing hosts

--- ssh.sh
+++ ssh.sh
@@ -51,7 +51,8 @@ else
             echo 'Authored by : "Behzad Fazelasl"'
             tput setaf 7;
             
-            echo "press [0] to config | press [1] to see the LIST | press [2] to add new host"
+            echo "press [0] to config | press [1] to see the LIST"
+            echo "press [2] to add new host | press [3] to manage existing"
             read status
             if [ '1' = $status ] >/dev/null 2>&1
                 then
@@ -80,6 +81,57 @@ else
                 gnome-terminal -- /bin/bash -c "nano database.conf"
                 PID=$!
                 kill -INT $PID
+            elif [ '3' = $status ] >/dev/null 2>&1  #delete hosts
+                then
+                mysql --host=$hostname --user=$db_username --password=$db_password --batch --silent --skip-column-names --execute="use $db_name; select id , username from bash.hosts;" | while read id username; do
+                    echo "$(echo $id - $username | toilet -f term -F border --gay)";
+                done 
+                
+                echo "Enter chosen host's ID :"
+                read id
+
+                username=$(mysql --host=localhost --user=behzad --password=dollar --batch --silent --execute="use bash; select username from bash.hosts where id='$id';") >/dev/null 2>&1
+                password=$(mysql --host=localhost --user=behzad --password=dollar --batch --silent --execute="use bash; select password from bash.hosts where id='$id';") >/dev/null 2>&1
+                ip=$(mysql --host=localhost --user=behzad --password=dollar --batch --silent --execute="use bash; select ip from bash.hosts where id='$id';") >/dev/null 2>&1
+                port=$(mysql --host=localhost --user=behzad --password=dollar --batch --silent --execute="use bash; select port from bash.hosts where id='$id';") >/dev/null 2>&1
+                
+                echo "press [0] to delete | press [1] to edit"
+                read action
+                if [ '0' = $action ] >/dev/null 2>&1
+                    then
+                    mysql -h localhost -ubehzad -pdollar -B -e "use bash; delete from hosts where id=$id;"
+                else
+                    echo "enter new credentials, leave it blank if you don't want it to change"
+
+                    echo "new username :"
+                    read new_username
+                    if [ -n "$new_username" ] 
+                        then
+                        mysql -h localhost -ubehzad -pdollar -B -e "use bash; update bash.hosts set username='$new_username' where id=$id;"   
+                    fi
+
+                    echo "new password :"
+                    read new_password
+                    if [ -n "$new_password" ] 
+                        then
+                        mysql -h localhost -ubehzad -pdollar -B -e "use bash; update bash.hosts set password='$new_password' where id=$id;"   
+                    fi
+
+                    echo "new ip :"
+                    read new_ip
+                    if [ -n "$new_ip" ] 
+                        then
+                        mysql -h localhost -ubehzad -pdollar -B -e "use bash; update bash.hosts set ip='$new_ip' where id=$id;"   
+                    fi
+
+                    echo "new port :"
+                    read new_port
+                    if [ -n "$new_port" ] 
+                        then
+                        mysql -h localhost -ubehzad -pdollar -B -e "use bash; update bash.hosts set port='$new_port' where id=$id;"   
+                    fi
+
+                fi
             else
                 echo "you have a new host to add,please enter credentials"
                 echo "username :"
